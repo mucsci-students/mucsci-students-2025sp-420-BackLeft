1673f73e1380f6da70e1a29e3aa6815a234c65f6
Tim King <163905685+TimKing35@users.noreply.github.com>
2025-02-11 17:00:32 -0500

    Removed 'methods' from Class
    There were unnecessary methods within the Class.java file which were not required by current specifications.

--- src/edu/millersville/backleft/Class.java
+++ src/edu/millersville/backleft/Class.java
@@ -6,13 +6,11 @@ import java.util.Map;
 public class Class {
     private final String className;
     private final Map<String, String> attributes; // Field name -> Type
-    private final Map<String, String> methods;    // Method name -> Return Type
 
     // Constructor for a 'Class' object
     public Class(String className) {
         this.className = className;
         this.attributes = new HashMap<>();
-        this.methods = new HashMap<>();
     }
 
     // Retrieves the name of an instance of 'Class'
@@ -25,19 +23,9 @@ public class Class {
         attributes.put(name, type);
     }
 
-    // Creates a new method within an instance of a 'Class'
-    public void addMethod(String name, String returnType) {
-        methods.put(name, returnType);
-    }
-
     // Returns the attributes contained within an instance of 'Class'
     public Map<String, String> getAttributes() {
         return new HashMap<>(attributes);
-    }
-
-    // Returns the methods contained within an instance of 'Class'
-    public Map<String, String> getMethods() {
-        return new HashMap<>(methods);
     }
 
     // Prints a 'Class' with all of its attributes and methods
